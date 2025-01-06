7e0fc5ad037ad334eac766bf8494bb7e5ff51e57
susisarvesh <susindransarvesh@gmail.com>
2025-01-06 08:16:57 +0530

    Input Field Done Add New Task

--- rec/src/Master/AddTaskModel.tsx
+++ rec/src/Master/AddTaskModel.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
 function AddTaskModal({ isOpen, onClose }) {
-  if (!isOpen) return null; // Don't render if the modal is closed
+  if (!isOpen) return null; 
+  const [date, setDate] = useState<Date | undefined>(new Date())
+
+
+
+
+
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="relative p-6 bg-white rounded-lg shadow-lg w-100">
-        <h2 className="mb-4 text-xl font-semibold">Add Task</h2>
+        <h2 className="mb-4 text-xl font-semibold">Add New Task</h2>
         <div className='flex'>
         <input
           type="text"
@@ -18,10 +31,44 @@ function AddTaskModal({ isOpen, onClose }) {
           placeholder="Add Description"
           className="w-full px-3 py-2 mb-4 border rounded h-[80px]"
           />
-          </div>
+        </div>
+         <div className="flex w-full mb-3 sm:w-auto">
+          <Select>
+            <SelectTrigger className="w-full sm:w-[190px]">
+              <SelectValue placeholder="Medium" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="alltask">All Task</SelectItem>
+              <SelectItem value="todo">To Do</SelectItem>
+              <SelectItem value="inprogress">In Progress</SelectItem>
+              <SelectItem value="completed">Completed</SelectItem>
+            </SelectContent>
+          </Select>
+          <Select>
+            <SelectTrigger className="w-full sm:w-[190px]">
+              <SelectValue placeholder="Select Task" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="alltask">All Task</SelectItem>
+              <SelectItem value="todo">To Do</SelectItem>
+              <SelectItem value="inprogress">In Progress</SelectItem>
+              <SelectItem value="completed">Completed</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-col">
+            <Calendar
+    mode="single"
+    selected={date}
+    onSelect={setDate}
+    className="border rounded-md"
+          />
+          <input type="text" placeholder="Assignee" className="w-full h-[50px] px-3 mr-2 py-2 mt-2 mb-4 border rounded" />
+          <input type="text" placeholder="Tags(Comma Seperated ',')" className="w-full h-[50px] px-3 mr-2 py-2 mb-4 border rounded" />
+        </div>
         <div className="flex justify-end gap-2">
           <button
-            className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+            className="px-4 py-2 text-white bg-black rounded hover:bg-gray-900"
             onClick={onClose}
           >
             Add Task
@@ -30,6 +77,8 @@ function AddTaskModal({ isOpen, onClose }) {
             className="px-4 py-2 text-gray-700 bg-gray-300 rounded hover:bg-gray-400"
             onClick={onClose}
           >
+          
+            
             Cancel
           </button>
         </div>
